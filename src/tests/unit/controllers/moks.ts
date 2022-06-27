a70743bb0c5765f599907c534d79fe322e286ea2
Igor Campos Moreira <igorcampos200@gmail.com>
2022-06-27 02:32:29 -0300

    teste test

---
+++ src/tests/unit/controllers/moks.ts
@@ -0,0 +1,64 @@
+export const carMoksdb = [
+  {
+		"_id": "62b8bda7bfcd1a2756852d66",
+		"model": "Ferrari Maranello",
+		"year": 1963,
+		"color": "red",
+		"buyValue": 3500000,
+		"doorsQty": 2,
+		"seatsQty": 2
+	},
+	{
+		"_id": "62b8be56bfcd1a2756852d69",
+		"model": "fiat Uno",
+		"year": 1963,
+		"color": "black",
+		"buyValue": 1000,
+		"doorsQty": 2,
+		"seatsQty": 5
+	}
+]
+// moto
+// model: 'Yamaha NEO',
+// year: 2022,
+// color: 'Blue',
+// buyValue: 9000,
+// category: 'Street',
+// engineCapacity: 125,
+
+export const motoMoksdb = [{
+  "_id": "62b8beffbfcd1a2756852d6c",
+  "model": "Honda CG Titan 125",
+  "year": 1963,
+  "color": "red",
+  "buyValue": 3500,
+  "category": "Street",
+  "engineCapacity": 125
+},
+{
+  "_id": "62b8bf22bfcd1a2756852d6e",
+  "model": "Honda CG Titan",
+  "year": 1985,
+  "color": "black",
+  "buyValue": 5500,
+  "category": "Street",
+  "engineCapacity": 150
+}]
+
+export const carOK = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const invalidCar = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 1
+}
--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,27 +1,97 @@
 // template para criação dos testes de cobertura da camada de controller
 
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import server from '../../../server';
+import CarService from '../../../services/CarService'
+import {carOK, invalidCar, carMoksdb} from './moks';
+import { request } from 'http';
+import CarController from '../../../controllers/CarController';
+import { assert } from 'console';
 
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe('Sua descrição', () => {
+describe('##Test Car controller##', () => {
+  
+  const carService = new CarService();
+  const carController = new CarController(carService);
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  const req = {
+    body: {},
+    params: {},
+  } as any;
 
-//   after(()=>{
-//     ().restore();
-//   })
+  const res = {
+    json: sinon.spy(),
+    status: sinon.stub().returns({ json: sinon.spy() }),
+  } as any
+  
+  describe('--> Test Create',async () => {
+    beforeEach(() => {
+        sinon.stub(carService, 'create');
+    });
+  
+    afterEach(()=>{
+      (carService.create as sinon.SinonStub).restore();
+    });
+    it('-> Invalid Car:: Verifica se um carro não é criado com sucesso', async () => {
+      req.body = invalidCar;
+      (carService.create as sinon.SinonStub).resolves({ error: 'Id must have 24 hexadecimal characters' });
+      await carController.create(req, res);
+      sinon.assert.calledWith(res.status, 400);
+      sinon.assert.calledWith(res.status(400).json, { error: 'Id must have 24 hexadecimal characters' });
+    });
 
-//   it('', async () => {});
+    it('-> Invalid request:: verifica se a requisição estiver incorreta o retorno é 500', async () => {
+      req.body = null;
+      (carService.create as sinon.SinonStub).resolves(null);
+      await carController.create(req, res);
+      sinon.assert.calledWith(res.status, 500);
+    })
 
-// });
+    it('-> Valid Car:: Verifica se um carro é criado com sucesso', async () => {
+      req.body = carOK;
+      (carService.create as sinon.SinonStub).resolves(carMoksdb[0]);
+      await carController.create(req, res);
+      sinon.assert.calledWith(res.status, 201);
+      sinon.assert.calledWith(res.status(201).json, carMoksdb[0]);
+    });
+  });
+
+    describe('--> Teste readOne', async () => {
+      beforeEach(async () => {
+        sinon.stub(carService, 'readOne');
+      }
+      )
+
+      afterEach(async()=>{
+        sinon.restore();
+      });
+
+      it('-> InvalidId:: verifica se levanta erro passando um id incorreto', async () => {
+        req.params = {id: '12345'};
+        (carService.readOne as sinon.SinonStub).resolves()
+        await carController.readOne(req, res);
+        sinon.assert.calledWith(res.status, 400);
+        sinon.assert.calledWith(res.status(400).json, {error: 'Id must have 24 hexadecimal characters'})
+      });
+      it('-> InternalError', async () => {
+        (carService.readOne as sinon.SinonStub).resolves();
+        await carController.readOne(req, res);
+        sinon.assert.calledWith(res.status, 500);
+        sinon.assert.calledWith(res.status(500).json, {error: 'Internal Server Error'})
+      });
+      // it('-> ValidId:: Retorno 200', async () => {
+      //   req.params = { id: '62b8bda7bfcd1a2756852d66'};
+      //   (carService.readOne as sinon.SinonStub).resolves();
+      //   await carController.readOne(req, res);
+      //   sinon.assert.calledWith(res.status, 200);
+      // })
+    })
+
+  });
